db0be4f0a9d489cf10cfcf650231dc6671aff028
Ole Mathias Heggem <olemathias.aa.heggem@gmail.com>
2023-12-18 01:07:25 +0100

    fixes

--- external-dns/external-dns.tf
+++ external-dns/external-dns.tf
@@ -6,7 +6,7 @@ locals {
 
     extraArgs = [
       "--pdns-server=${var.pdns_server}",
-      "--txt-prefix=k8s"
+      "--txt-prefix=k8s-"
     ]
     domainFilters = var.domain_filters
     env = [{ name = "EXTERNAL_DNS_PDNS_API_KEY"
